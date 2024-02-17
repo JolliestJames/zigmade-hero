2b851cb77ee901bb7d319c46ac9eee2c9bde391a
JolliestJames <cloudismydog@gmail.com>
2024-02-16 20:46:30 -0800

    day 3 complete, allocate the backbuffer

--- src/win32_handmade.zig
+++ src/win32_handmade.zig
@@ -12,7 +12,71 @@ const win32 = struct {
     usingnamespace @import("win32").system.diagnostics.debug;
 };
 
-fn MainWindowCallback(
+// TODO: Use a global State struct for now
+const State = struct {
+    var running: bool = undefined;
+    var bitmap_info: win32.BITMAPINFO = undefined;
+    var bitmap_memory: ?*anyopaque = undefined;
+    var bitmap_handle: ?win32.HBITMAP = undefined;
+    var device_context: win32.HDC = undefined;
+};
+
+fn win32ResizeDIBSection(width: i32, height: i32) !void {
+    // TODO: Maybe don't free first, free after, then free first if that fails
+    if (State.bitmap_handle != undefined) {
+        _ = win32.DeleteObject(State.bitmap_handle);
+    }
+
+    if (State.device_context == undefined) {
+        // TODO: should we recreate DC under certain special circumstances?
+        State.device_context = win32.CreateCompatibleDC(null);
+    }
+
+    State.bitmap_info.bmiHeader.biSize = @sizeOf(@TypeOf(State.bitmap_info.bmiHeader));
+    State.bitmap_info.bmiHeader.biWidth = width;
+    State.bitmap_info.bmiHeader.biHeight = height;
+    State.bitmap_info.bmiHeader.biPlanes = 1;
+    State.bitmap_info.bmiHeader.biBitCount = 32;
+    State.bitmap_info.bmiHeader.biCompression = win32.BI_RGB;
+
+    State.bitmap_handle = win32.CreateDIBSection(
+        State.device_context,
+        &State.bitmap_info,
+        win32.DIB_RGB_COLORS,
+        &State.bitmap_memory,
+        null,
+        0,
+    );
+
+    _ = win32.ReleaseDC(null, State.device_context);
+}
+
+fn win32UpdateWindow(
+    device_context: ?win32.HDC,
+    _: win32.HWND,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+) !void {
+    _ = win32.StretchDIBits(
+        device_context,
+        x,
+        y,
+        width,
+        height,
+        x,
+        y,
+        width,
+        height,
+        State.bitmap_memory,
+        &State.bitmap_info,
+        win32.DIB_RGB_COLORS,
+        win32.SRCCOPY,
+    );
+}
+
+fn win32MainWindowCallback(
     window: win32.HWND,
     message: u32,
     wParam: win32.WPARAM,
@@ -22,36 +86,35 @@ fn MainWindowCallback(
 
     switch (message) {
         win32.WM_SIZE => {
-            win32.OutputDebugStringA("WM_SIZE\n");
-        },
-        win32.WM_DESTROY => {
+            var client_rect = std.mem.zeroInit(win32.RECT, .{});
+            _ = win32.GetClientRect(window, &client_rect);
+
+            var width = client_rect.right - client_rect.left;
+            var height = client_rect.bottom - client_rect.top;
+
+            try win32ResizeDIBSection(width, height);
             win32.OutputDebugStringA("WM_SIZE\n");
         },
         win32.WM_CLOSE => {
-            win32.OutputDebugStringA("WM_CLOSE\n");
+            // TODO: Handle with a message to the user?
+            State.running = false;
         },
         win32.WM_ACTIVATEAPP => {
             win32.OutputDebugStringA("WM_ACTIVATEAPP\n");
         },
+        win32.WM_DESTROY => {
+            // TODO: Handle as an error--recreate window?
+            State.running = false;
+        },
         win32.WM_PAINT => {
             var paint = std.mem.zeroInit(win32.PAINTSTRUCT, .{});
-
             var device_context = win32.BeginPaint(window, &paint);
             var x = paint.rcPaint.left;
             var y = paint.rcPaint.top;
             var height = paint.rcPaint.bottom - paint.rcPaint.top;
             var width = paint.rcPaint.right - paint.rcPaint.left;
 
-            const state = struct {
-                var operation: win32.ROP_CODE = win32.WHITENESS;
-            };
-            _ = win32.PatBlt(device_context, x, y, width, height, state.operation);
-
-            if (state.operation == win32.WHITENESS) {
-                state.operation = win32.BLACKNESS;
-            } else {
-                state.operation = win32.WHITENESS;
-            }
+            try win32UpdateWindow(device_context, window, x, y, width, height);
 
             _ = win32.EndPaint(window, &paint);
         },
@@ -74,7 +137,7 @@ pub export fn wWinMain(
 
     // TODO: does HREDRAW/OWNDC/VREDRAW still matter
     window_class.style = @enumFromInt(@intFromEnum(win32.CS_OWNDC) | @intFromEnum(win32.CS_HREDRAW) | @intFromEnum(win32.CS_VREDRAW));
-    window_class.lpfnWndProc = @ptrCast(&MainWindowCallback);
+    window_class.lpfnWndProc = @ptrCast(&win32MainWindowCallback);
     window_class.hInstance = instance;
     // window_class.hIcon = ;
     window_class.lpszClassName = win32.L("HandmadeHeroWindowClass");
@@ -94,9 +157,11 @@ pub export fn wWinMain(
             instance,
             null,
         )) |_| {
+            State.running = true;
+
             var message = std.mem.zeroInit(win32.MSG, .{});
 
-            while (true) {
+            while (State.running) {
                 var result = win32.GetMessageW(&message, null, 0, 0);
 
                 if (result > 0) {
