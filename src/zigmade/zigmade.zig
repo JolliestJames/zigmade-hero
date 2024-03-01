9e6cc10950b9f9cf81a6ef1d8306f72667780460
JolliestJames <cloudismydog@gmail.com>
2024-03-01 11:48:15 -0800

    day 12, properly index many-item pointers, begin pulling sound concerns into the platform API

--- src/zigmade/zigmade.zig
+++ src/zigmade/zigmade.zig
@@ -1,14 +1,55 @@
+const std = @import("std");
+
 // TODO: Services that the platform layer provides to the game
 
 // NOTE: Services that the game provides to the platform layer
 // maybe expand in the future - sound on separate thread
 
+// TODO: rendering will become a three-tiered abstraction
 pub const GameOffscreenBuffer = struct {
     memory: ?*anyopaque = undefined,
     width: i32 = undefined,
     height: i32 = undefined,
     pitch: i32 = undefined,
 };
+
+pub const SoundBuffer = struct {
+    samples_per_second: i32 = undefined,
+    sample_count: i32 = undefined,
+    samples: [*]i16 = undefined,
+};
+
+fn game_output_sound(
+    sound_buffer: *SoundBuffer,
+    tone_hertz: i32,
+) !void {
+    const t_sine = struct {
+        var value: f32 = undefined;
+    };
+
+    const tone_volume: i16 = 3_000;
+    var wave_period = @divTrunc(sound_buffer.samples_per_second, tone_hertz);
+    var sample_out: [*]i16 = sound_buffer.samples;
+
+    for (0..@as(usize, @intCast(sound_buffer.sample_count))) |i| {
+        var sine_value: f32 = std.math.sin(t_sine.value);
+        var sample_value: i16 = @as(i16, @intFromFloat(
+            sine_value * @as(
+                f32,
+                @floatFromInt(tone_volume),
+            ),
+        ));
+
+        sample_out[2 * i] = sample_value;
+        sample_out[2 * i + 1] = sample_value;
+
+        t_sine.value +=
+            2.0 *
+            std.math.pi *
+            1.0 /
+            @as(f32, @floatFromInt(wave_period));
+    }
+}
 
 fn render_weird_gradient(
     buffer: *GameOffscreenBuffer,
@@ -21,23 +62,16 @@ fn render_weird_gradient(
         var pixel: [*]u32 = @ptrCast(@alignCast(row));
 
         for (0..@intCast(buffer.width)) |x| {
-            var blue: u32 = @as(
-                u8,
-                @truncate(x + @as(
-                    u32,
-                    @bitCast(blue_offset),
-                )),
-            );
-            var green: u32 = @as(
-                u8,
-                @truncate(y + @as(
-                    u32,
-                    @bitCast(green_offset),
-                )),
-            );
+            var blue: u32 = @as(u8, @truncate(x + @as(
+                u32,
+                @bitCast(blue_offset),
+            )));
+            var green: u32 = @as(u8, @truncate(y + @as(
+                u32,
+                @bitCast(green_offset),
+            )));
 
-            pixel[0] = (green << 8) | blue;
-            pixel += 1;
+            pixel[x] = (green << 8) | blue;
         }
 
         row += @as(usize, @intCast(buffer.pitch));
@@ -50,12 +84,16 @@ fn render_weird_gradient(
 // - bitmap buffer
 // - sound buffer
 pub fn game_update_and_render(
-    buffer: *GameOffscreenBuffer,
+    offscreen_buffer: *GameOffscreenBuffer,
     blue_offset: i32,
     green_offset: i32,
+    sound_buffer: *SoundBuffer,
+    tone_hertz: i32,
 ) !void {
+    // TODO: Allow sample offsets here for more robust platform options
+    try game_output_sound(sound_buffer, tone_hertz);
     try render_weird_gradient(
-        buffer,
+        offscreen_buffer,
         blue_offset,
         green_offset,
     );
